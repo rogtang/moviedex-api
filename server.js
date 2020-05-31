4aedc0f41253a89d302f0b7da9fc2895f57a16a4
rogtang <rogtang@gmail.com>
2020-05-31 12:08:29 -0700

    prepare for deploy

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const MOVIEDATA = require('./movies-data-small.json')
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
 app.use(function validateBearerToken(req, res, next) {
-  console.log('validate bearer token middleware')
   const apiToken = process.env.API_TOKEN
   const authToken = req.get('Authorization')
 
@@ -40,9 +40,18 @@ app.get('/movie', function getMovie(req, res) {
   res.json(response)
 })
 
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
 
 
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
